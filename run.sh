67776aed602694346fcc354bd733ea537b734c2a
Oscar Doral <oscar.doral@gmail.com>
2024-06-25 20:12:54 +0200

    Filter commits and tags for executions without changes apart from UID and DTSTAMP fields.

--- run.sh
+++ run.sh
@@ -11,14 +11,19 @@ echo "Build"
 echo "Run"
 java -jar ./sportcalendars-web-scraper/build/libs/sportcalendars-web-scraper-all.jar -pd=./ -gr
 
-echo "Configure git"
-git config --global user.name 'github-actions[bot]'
-git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+if ! git diff --unified=0 | grep -e "^[+-][^-+]" | grep -ve "^.DTSTAMP\|^.UID"; then
+  echo "No changes in existing events."
+else
+  echo "New events detected!"
+  echo "Configure git"
+  git config --global user.name 'github-actions[bot]'
+  git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 
-echo "Commit"
-git commit -a -m "[AUTOMATION] ${TIMESTAMP} execution"
-git push origin master
-git tag "${TIMESTAMP}"
-git push origin --tags
+  echo "Commit"
+  git commit -a -m "[AUTOMATION] ${TIMESTAMP} execution"
+  git push origin master
+  git tag "${TIMESTAMP}"
+  git push origin --tags
+fi
 
 echo "Done!"
