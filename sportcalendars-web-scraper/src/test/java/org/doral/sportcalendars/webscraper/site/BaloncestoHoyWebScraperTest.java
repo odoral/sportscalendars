a4a56ffe177a24cefa7586f5726951f4049811e1
oscar <oscar.doral@gmail.com>
2026-06-03 01:07:39 +0200

    [200~Basket events are not parsed

--- sportcalendars-web-scraper/src/test/java/org/doral/sportcalendars/webscraper/site/BaloncestoHoyWebScraperTest.java
+++ sportcalendars-web-scraper/src/test/java/org/doral/sportcalendars/webscraper/site/BaloncestoHoyWebScraperTest.java
@@ -20,8 +20,7 @@ import static org.mockito.Mockito.when;
  */
 class BaloncestoHoyWebScraperTest {
 
-    public static final String AMISTOSO = "Amistoso";
-    public static final String AMISTOSO_FEMENINO = "Amistoso Femenino";
+    public static final String LIGA_ENDESA = "Liga Endesa";
     public static final String NBA = "NBA";
     protected URL url;
     protected BaloncestoHoyWebScraper scraper;
@@ -38,14 +37,11 @@ class BaloncestoHoyWebScraperTest {
         Map<String, Calendar> result = scraper.parsePage(url);
 
         assertNotNull(result);
-        assertTrue(result.containsKey(AMISTOSO));
-        assertEquals(2, result.get(AMISTOSO).getEvents().size());
-
-        assertTrue(result.containsKey(AMISTOSO_FEMENINO));
-        assertEquals(2, result.get(AMISTOSO_FEMENINO).getEvents().size());
-
         assertTrue(result.containsKey(NBA));
-        assertEquals(1, result.get(NBA).getEvents().size());
+        assertEquals(2, result.get(NBA).getEvents().size());
+
+        assertTrue(result.containsKey(LIGA_ENDESA));
+        assertEquals(8, result.get(LIGA_ENDESA).getEvents().size());
     }
 
     @Test
